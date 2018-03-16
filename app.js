ff6ed01cddd57489eed5f58a4f8454e5b06b38ae
IsuruNana <isurunanayakkara505@gmail.com>
2018-03-16 14:26:55 -0500

    Added missing notes import

--- app.js
+++ app.js
@@ -3,21 +3,4 @@ console.log('Starting app.js');
 const fs = require('fs');
 const _ = require('lodash');
 
-
-
-//console.log(_.isString('Bateman'));
-
-let filteredArray = _.uniq([
-    'Hey', 'hey', 'monica', 'hey', 'poday', 1, 3, 5, 1
-].map(word => {
-    return word.toString().toLowerCase();
-}));
-console.log(filteredArray);
-
-// const user = os.userInfo();
-
-// fs.writeFile('greetings.txt', `Hey ${user.username} you ${notes.age} year old boi`, (err) => {
-//     if(err) {
-//         console.log('Write failed');
-//     }
-// });
+const notes = require('./notes.js');
