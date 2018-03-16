19c1fe07b2405b54b8f65800e89837384f9bec74
IsuruNana <isurunanayakkara505@gmail.com>
2018-03-16 14:37:06 -0500

    Added command line arguments process logic

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-console.log('Starting app.js');
+//console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+let command = process.argv[2];
+let data = process.argv[3];
+
+switch(command) {
+    case 'add':
+        break;
+    case 'list':
+        break;
+    case 'read':
+        break;
+    case 'remove':
+        break;
+    default:
+        break;
+}
+
+console.log(data);
